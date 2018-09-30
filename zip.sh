cbbba55ec7434f2011792503ee0b58c2678f94ce
Barnabas Agoston <agostonbarna@gmail.com>
2018-09-30 20:38:24 +0200

    Change the location of the release zips

--- zip.sh
+++ zip.sh
@@ -1,3 +1,11 @@
 #/bin/bash
 
-zip -r UniqueTabs.zip src
+version=$(git grep -Poh --no-line-number '"version": *"\K[^"]+' src/manifest.json)
+dst=dst/$version.zip
+
+if [[ -f $dst ]]; then
+  echo "$version.zip already exists!"
+  exit 1
+else
+  git archive -v -o $dst HEAD:src
+fi
